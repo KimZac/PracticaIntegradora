a50192b096bfdd87a5e135d2787d4dd0f9f2aeb4
kimzac <kimzack@gmail.com>
2023-05-18 13:04:29 -0600

    A modifico Robin.js

--- Robin.js
+++ Robin.js
@@ -1,2 +1,4 @@
 Robin tiene 8 MessageEvent, es un cachorro y es muy jugueton
 Ademas, es hermano menor de Zuko y es muy latoso 
+
+Robin es un perro adoptado pero muy amado, tiene muchos juguetes y le encanta robar calcetines.
